890573936d204e949f200c55fb0af9a442babf4b
Farid Shahy <fshahy@gmail.com>
2015-12-03 09:55:39 +0330

    [IMP] init website_seo_module (#20)

---
+++ website_seo_sale/__openerp__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'eCommerce SEO Optimized',
+    'category': 'Website',
+    'sequence': 55,
+    'summary': 'Sell Your Products Online',
+    'website': 'http://www.bloopark.de',
+    'version': '1.0',
+    'description': """
+OpenERP E-Commerce SEO Optimized
+==================
+
+        """,
+    'depends': [ 'website_sale',],
+    'data': [
+        'views/templates.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': ['static/src/xml/*.xml'],
+    'installable': True,
+    'application': True,
+}
